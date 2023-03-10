fc2c9074123ffdfc069fae7282618e094336f50d
ricardocastellr <rcastell@ucol.mx>
2023-03-09 20:29:13 -0600

    Primer ejercicio

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express'); 
+var app = express();
+var port = process.env.PORT || 3000;
+app.use('/assets', express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.use('/', function(req,res,next){
+    console.log('Request Url:' + req.url);
+    next();
+});
+app.get('/',function (req,res){
+    res.render('index');
+});
+app.get('/person/:id', function (req,res){
+    res.render('person', { numero: req.params.id });
+});
+
+app.listen(port);
