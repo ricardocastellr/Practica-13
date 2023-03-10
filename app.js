ee612658c4c5e5005445e9541ebb07c0336967f2
ricardocastellr <rcastell@ucol.mx>
2023-03-09 23:17:27 -0600

    Segundo ejercicio

--- app.js
+++ app.js
@@ -13,5 +13,12 @@ app.get('/',function (req,res){
 app.get('/person/:id', function (req,res){
     res.render('person', { numero: req.params.id });
 });
-
+let data = [
+    {id: 1, nombre: "Hugo", apellido: "Estreda Ramirez", edad: 19},
+    {id: 2, nombre: "Estela", apellido: "Perez Suarez", edad: 18},
+    {id: 3, nombre: "Sabrina", apellido: "Contreras Morales", edad: 17},
+];
+app.get('/personas/', function (req,res){
+    res.render('personas', { data });
+});
 app.listen(port);
